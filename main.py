73902f6420f109681e4c8312e6db2849b95b0883
eduardo <eduardovanbenthum@gmail.com>
2025-10-24 14:38:46 +0200

    gerda

--- main.py
+++ main.py
@@ -2,21 +2,117 @@ from enum import Enum
 from typing import List, Tuple
 
 class EdgeType(Enum):
-    NORMAL = 0
-    SPECIAL = 1
-    DANGEROUS = 2
+    PEDESTRIAN = 0
+    BUS = 1
+    BOTH = 2
 
-def get_input() -> Tuple[int, List[Tuple[int, int, EdgeType]]]:
+
+class Edge:
+    def __init__(self, vertex1: int, vertex2: int, edge_type: EdgeType):
+        self.vertex1 = vertex1
+        self.vertex2 = vertex2
+        self.edge_type = edge_type
+        self.available = True
+
+    def can_use(self, other: EdgeType) -> bool:
+        return self.edge_type == EdgeType.BOTH or other == EdgeType.BOTH or self.edge_type == other
+
+
+class Vertex:
+    def __init__(self, vertex_id: int):
+        self.vertex_id = vertex_id
+        self.edges = []
+
+    def add_edge(self, edge: Edge):
+        self.edges.append(edge)
+
+
+def get_input() -> List[Vertex]:
     number_of_vertices, number_of_edges = map(int, input().split())
-    edges = [(a, b, EdgeType(t)) for a, b, t in
-             (map(int, input().split()) for _ in range(number_of_edges))]
-    return number_of_vertices, edges
+    vertices = [Vertex(i) for i in range(number_of_vertices)]
+
+    # add every edge to both vertices
+    for _ in range(number_of_edges):
+        v1, v2, edge_type_int = map(int, input().split())
+        edge = Edge(v1, v2, EdgeType(edge_type_int))
+
+        vertices[v1].add_edge(edge)
+        vertices[v2].add_edge(edge)
+
+    return vertices
+
+
+def cycle_search(vertices: List[Vertex]) -> int:
+    # loop over all vertices
+    result = 0
+    for vertex in vertices:
+        # per vertex check if there are any cycles for bus and pedestrian. If so, remove the first edge that is not "both". If all are "both" remove the first edge.
+        # check for pedestrian first
+        result += remove_edges_from_vertex(vertex, vertices, EdgeType.PEDESTRIAN)
+        result += remove_edges_from_vertex(vertex, vertices, EdgeType.BUS)
+
+    return result
+
+
+def remove_edges_from_vertex(vertex: Vertex, vertices: List[Vertex], edge_type: EdgeType) -> int:
+    cycles = get_all_cycles(vertex, vertices, edge_type)
+    result = 0
+    for cycle in cycles:
+        removed = False
+        for edge in cycle:
+            if edge.edge_type == EdgeType.PEDESTRIAN:
+                edge.available = False
+                removed = True
+                break
+
+        # apparently all edges in the cycle are "both", so we just remove the first one.
+        if not removed:
+            cycle[0].available = False
+
+        # update the counter + 1 for each edge that we "throw away".
+        result += 1
+
+    return result
+
+
+def get_all_cycles(start_vertex: Vertex, vertices: List[Vertex], edge_type: EdgeType) -> List[List[Edge]]:
+    visited = set()
+    edge_stack = []
+    cycles = []
+
+    def dfs(current: Vertex):
+        visited.add(current.vertex_id)
+
+        for edge in current.edges:
+            # skip invalid edges
+            if not edge.available or not edge.can_use(edge_type):
+                continue
+
+            # get neighbor
+            neighbor_id = edge.vertex2 if edge.vertex1 == current.vertex_id else edge.vertex1
+            neighbor = vertices[neighbor_id]
+
+            # traverse deeper
+            if neighbor.vertex_id not in visited:
+                edge_stack.append(edge)
+                dfs(neighbor)
+                edge_stack.pop()
+
+            # if we reach the start vertex again, it's a cycle
+            elif neighbor == start_vertex and edge not in edge_stack:
+                    # copy the stack and add the last edge
+                    cycles.append(edge_stack[:] + [edge])
+
+    dfs(start_vertex)
+    return cycles
 
 
 def main():
-    # add swallow function
-    input = get_input()
-    print(input)
+    vertices = get_input()
+
+    output = cycle_search(vertices)
+
+    print(output)
 
 if __name__ == '__main__':
     main()
--- main2.py
+++ main2.py
@@ -1,29 +1,39 @@
 from enum import Enum
-from typing import List, Tuple
+from copy import deepcopy
+from typing import List, Tuple, Any
+
 
 class EdgeType(Enum):
     WALKING = 0
     BUS = 1
     BOTH = 2
 
+
 def get_input() -> Tuple[int, List[Tuple[int, int, EdgeType]]]:
     number_of_vertices, number_of_edges = map(int, input().split())
     edges = [(a, b, EdgeType(t)) for a, b, t in
              (map(int, input().split()) for _ in range(number_of_edges))]
     return number_of_vertices, edges
+
+
 class SetUnion:
-    def __init__(self, number_of_vertices):
+    def __init__(self, number_of_vertices: int) -> None:
         # each vertex is its own group.
         self.parent = list(range(number_of_vertices))
         # how many seperate vertices groups do we have. At start its the amount of vertex.
         self.number_of_groups = number_of_vertices
-    def find(self, vertex):
-        if vertex == self.parent[vertex]:
-            return vertex
-        else:
-            return self.find(self.parent[vertex]) 
 
-    def union(self, start, end):
+
+    def find(self, vertex: int) -> int:
+        root = vertex
+        # Step 1: find the root
+        while root != self.parent[root]:
+            root = self.parent[root]
+
+        return root
+
+
+    def union(self, start: int, end: int) -> bool:
         parent_start = self.find(start)
         parent_end = self.find(end)
         if parent_start == parent_end:
@@ -33,37 +43,54 @@ class SetUnion:
         self.number_of_groups -= 1
         return True
 
-def find_most_closed_roads(V,E):
-    walking_union = SetUnion(V)
-    bus_union = SetUnion(V)
+
+    def __deepcopy__(self, memo: dict[int, Any]) -> "SetUnion":
+        # Create a new instance without calling __init__
+        new_set_union = self.__class__.__new__(self.__class__)
+        # Deep copy all attributes manually
+        new_set_union.parent = deepcopy(self.parent, memo)
+        new_set_union.number_of_groups = self.number_of_groups
+        return new_set_union
+
+
+def find_most_closed_roads(vertices: int, edges: List[Tuple[int, int, EdgeType]]) -> int:
+    both_union = SetUnion(vertices)
     used_roads = 0
-    amount_of_roads = len(E)
+    amount_of_roads = len(edges)
     #Check for both
-    for start,end,road in E:
-        if(road == EdgeType.BOTH):
-            used_bus = bus_union.union(start,end)
-            used_walking = walking_union.union(start,end)
-            if used_bus or used_walking:
+    for start,end,road in edges:
+        if road == EdgeType.BOTH:
+            used_both = both_union.union(start,end)
+            if used_both:
                 used_roads +=1
+
+    #copy current union
+    walking_union = deepcopy(both_union)
+    bus_union = both_union
+
     #Check walking
-    for start,end,road, in E:
-        if(road == EdgeType.WALKING):
+    for start,end,road, in edges:
+        if road == EdgeType.WALKING:
             used_walking = walking_union.union(start,end)
             if used_walking:
                 used_roads +=1
     #Check bus
-    for start,end,road, in E :
-        if(road == EdgeType.BUS):
+    for start,end,road, in edges :
+        if road == EdgeType.BUS:
             used_bus = bus_union.union(start,end)
             if used_bus:
                 used_roads +=1
     
     if bus_union.number_of_groups != 1 or walking_union.number_of_groups != 1:
         return -1
+
     return amount_of_roads - used_roads
+
+
 def main():
     V,E = get_input()
     print(find_most_closed_roads(V,E))
+
 
 if __name__ == '__main__':
     main()
